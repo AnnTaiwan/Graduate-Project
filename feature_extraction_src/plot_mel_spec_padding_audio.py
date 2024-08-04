a68940d04cbaee317f029f56479d18b53c0da3dd
Ann <ann20021007@gmail.com>
2024-08-04 04:35:27 +0100

    Two different padding way, one is no padding, and the other is padding with original audio

---
+++ feature_extraction_src/plot_mel_spec_padding_audio.py
@@ -0,0 +1,39 @@
+'''
+Use original audio to pad the audio to 5 seconds
+'''
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from observe_audio_function_ver3 import SAMPLE_RATE, AUDIO_LEN, load_audio, get_mel_spectrogram, plot_mel_spectrogram
+import os 
+# Create a directory to store the spectrogram images
+source_folder = r"D:\clone_audio\chinese_audio_dataset_ver3\spoof\test_audio"
+destination_folder = r"D:\clone_audio\chinese_audio_dataset_ver3\test_mel_spec_padding_original_audio"
+
+if __name__ == "__main__":
+    os.makedirs(destination_folder, exist_ok=True)
+
+    for filename in os.listdir(source_folder):
+        filepath = os.path.join(source_folder, filename)
+        audio, sr = load_audio(filepath, sr=SAMPLE_RATE)
+        # pad the audio with the original audio or cut the audio
+        if len(audio) < AUDIO_LEN:
+            length_audio = len(audio)
+            repeat_count = (AUDIO_LEN + length_audio - 1) // length_audio  # Calculate the `ceiling` of AUDIO_LEN / length_audio
+            audio = np.tile(audio, repeat_count)[:AUDIO_LEN]  # Repeat and cut to the required length
+        else:
+            audio = audio[:AUDIO_LEN]
+
+        spec = get_mel_spectrogram(audio)
+        fig = plot_mel_spectrogram(spec)
+        plt.title("Mel-Spectrogram", fontsize=17)
+
+        # Save the spectrogram image with a meaningful filename
+        # dest_filename = f"bonafide_melspec_{filename[:-4]}.png"  # Use single quotes inside the f-string
+        dest_filename = f"spoof_melspec_{filename[:-4]}.png"  # Use single quotes inside the f-string
+        dest_filepath = os.path.join(destination_folder, dest_filename)
+        plt.savefig(dest_filepath)
+        # Close the figure to free up resources
+        plt.close()
+
+    print(f"Spectrogram images saved to {destination_folder}")
